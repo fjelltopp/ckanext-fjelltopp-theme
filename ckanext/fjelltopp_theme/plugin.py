65a4b53be5a69e94d9e16a98eef307f53e677e07
toavina <toavina@fjelltopp.org>
2024-07-19 17:35:03 +0300

    Fjelltopp theme is now working
    Added public and assets in plugin.py, moved webassets.yml and all css files to assets folder

--- ckanext/fjelltopp_theme/plugin.py
+++ ckanext/fjelltopp_theme/plugin.py
@@ -14,9 +14,6 @@ class FjelltoppThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
 
     def update_config(self, config: CKANConfig):
-
-        # Add this plugin's templates dir to CKAN's extra_template_paths, so
-        # that CKAN will use this plugin's custom templates.
-        # 'templates' is the path to the templates dir, relative to this
-        # plugin.py file.
         toolkit.add_template_directory(config, "templates")
+        toolkit.add_public_directory(config, 'public')
+        toolkit.add_resource('assets', 'fjelltopp-theme')
