e077a1701ad24c4fa92febd9e8c77f71d67d5838
Toavina A. <toavinahajaina@gmail.com>
2024-11-21 12:04:01 +0300

    133 - Configure banner for staging and dev (#44)
    * Banner is configurable
    
    * Finalized
    
    * Hides banner for production
    
    * Moved the banner classes to a separate scss file
    
    * Added comment to helper
    
    * Refactoring
    
    * Small change for hover styling
    
    ---------
    
    Co-authored-by: toavina <toavina@fjelltopp.org>

--- ckanext/fjelltopp_theme/helpers.py
+++ ckanext/fjelltopp_theme/helpers.py
@@ -104,3 +104,13 @@ def get_license(license_id):
             return license
     else:
         return {}
+
+
+def get_deployment_info(default='development'):
+    """
+    possible values are development, staging and production, otherwise add more css classes
+    """
+    return {
+            "type": toolkit.config.get('ckan.deployment_env.type', default),
+            "message": toolkit.config.get('ckan.deployment_env.banner_message', "BETA - Site under development")
+         }
